6856bbcb7e440eae1582d6cb21abe0e2d39b2a92
sebas2180 <54959701+sebas2180@users.noreply.github.com>
2020-04-24 14:10:37 -0300

    24/04

--- FrontEnd/src/app/services/authservice.service.ts
+++ FrontEnd/src/app/services/authservice.service.ts
@@ -7,7 +7,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AuthserviceService {
   //public ruta :string  = `http://localhost:2100/`;
-  public ruta :string  = `http://134.122.13.83:2000/`;
+  public ruta :string  = `http://161.35.134.242:2000/`;
 
   public clearLocalStorage(){
     localStorage.removeItem('userInfo');
